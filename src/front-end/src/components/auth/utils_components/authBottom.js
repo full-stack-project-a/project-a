c1ddcc93576b9d4a6297339fe4c8e904554f01e8
Yanru Chen <47041483+yanruc123@users.noreply.github.com>
2023-11-28 17:32:01 -0800

    changed css scope for auth form, fixed styling

--- src/front-end/src/components/auth/utils_components/authBottom.js
+++ src/front-end/src/components/auth/utils_components/authBottom.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import "../../../styles/auth/auth_bottom.css";
 
 const AuthBottom = ({ currPage }) => {
+   const navigate = useNavigate();
+
+   const navigateAndReplace = (path) => {
+      navigate(path, { replace: true });
+   };
+
    return (
       <div className="bottom-group">
          {currPage === 'signin' && (
             <>
                <p id="top">
                   Don't have an account?{" "}
-                  <a href="/signup" className="link">Sign up</a>
+                  <span className="link" onClick={() => navigateAndReplace('/signup')}>Sign up</span>
                </p>
                <p>
-                  <a href="/forgot-password" className="link">Forget password?</a>
+                  <span className="link" onClick={() => navigateAndReplace('/updatePassword')}>Forget password?</span>
                </p>
             </>
          )}
@@ -19,7 +26,7 @@ const AuthBottom = ({ currPage }) => {
          {currPage === 'signup' && (
             <p>
                Already have an account?{" "}
-               <a href="/signin" className="link">Sign in</a>
+               <span className="link" onClick={() => navigateAndReplace('/signin')}>Sign in</span>
             </p>
          )}
 
