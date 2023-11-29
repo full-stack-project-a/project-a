26e457c78c069ac636aeac049ba9ae2ed926c249
Zhengmao Zhang <malker@qq.com>
2023-11-28 18:48:11 -0800

    update routing list and doc

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import AuthComponent from './components/auth/AuthComponent';
+import ProductPage from './components/product/ProductPage';
+import DetailPage from './components/product/DetailPage';
+import Formtable from './components/product/Formtable';
 // import Formtable from './components/product/Formtable';
 // import ProductPage from './components/product/ProductPage';
 // import ProductCard from './components/product/CardStyle';
@@ -20,6 +23,9 @@ function App() {
                <Route path="/signin" element={<AuthComponent currPage="signin" />} />
                <Route path="/signup" element={<AuthComponent currPage="signup" />} />
                <Route path="/updatePassword" element={<AuthComponent currPage="updatePassword" />} />
+               <Route path="/products" element={<ProductPage/>} />
+               <Route path="/products/1" element={<DetailPage/>} />
+               <Route path="/products/new" element={<Formtable/>} />
                {/* Add more routes as needed */}
             </Routes>
             <Footer />
