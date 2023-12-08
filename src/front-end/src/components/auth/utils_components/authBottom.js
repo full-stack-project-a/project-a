970c2f85d86085f234d064e0e2dabe2a0c31ac69
Yanru Chen <47041483+yanruc123@users.noreply.github.com>
2023-12-08 11:07:35 +0800

    added vendor option to frontend

--- src/front-end/src/components/auth/utils_components/authBottom.js
+++ src/front-end/src/components/auth/utils_components/authBottom.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from "../../../styles/auth/auth_bottom.module.css";
 
 const AuthBottom = ({ currPage }) => {
    const navigate = useNavigate();
+   const [isVendor, setIsVendor] = useState(false);
 
    const navigateAndReplace = (path) => {
       navigate(path, { replace: true });
+   };
+
+   const handleCheckboxChange = () => {
+      setIsVendor(!isVendor);
    };
 
    return (
@@ -24,10 +29,20 @@ const AuthBottom = ({ currPage }) => {
          )}
 
          {currPage === 'signup' && (
-            <p className={styles.bottomGroupText}>
-               Already have an account?{" "}
-               <span className={styles.link} onClick={() => navigateAndReplace('/signin')}>Sign in</span>
-            </p>
+            <>
+               <p className={styles.bottomGroupText}>
+                  Already have an account?{" "}
+                  <span className={styles.link} onClick={() => navigateAndReplace('/signin')}>Sign in</span>
+               </p>
+               <div className={styles.vendorBox}>
+                  <input
+                     type="checkbox"
+                     checked={isVendor}
+                     onChange={handleCheckboxChange}
+                  />
+                  <label>vendor?</label>
+               </div>
+            </>
          )}
 
          {/* No content for update password page */}
