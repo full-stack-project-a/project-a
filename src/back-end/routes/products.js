bce52d9776ef070b4c140119b9d9d733f659d54e
Yanru Chen <47041483+yanruc123@users.noreply.github.com>
2023-12-12 17:20:34 +0800

    added frontend route protection & docs

--- src/back-end/routes/products.js
+++ src/back-end/routes/products.js
@@ -2,11 +2,11 @@ const express = require('express');
 const Product = require('../models/product');
 const router = express.Router();
 const productController = require('../controllers/productController');
-var { verifyTokenAndRole } = require("../controllers/userController");
+var { authenticateToken, verifyTokenAndRole } = require("../controllers/userController");
 
-router.post('/', verifyTokenAndRole("vendor"), productController.createProduct);
+router.post('/', authenticateToken, verifyTokenAndRole("vendor"), productController.createProduct);
 router.get('/:id', verifyTokenAndRole("public"), productController.getProductByID);
-router.put('/:id', verifyTokenAndRole("vendor"),productController.editProduct);
+router.put('/:id', authenticateToken, verifyTokenAndRole("vendor"),productController.editProduct);
 router.get('/', verifyTokenAndRole("public"), productController.getProducts);
 
 module.exports = router;
